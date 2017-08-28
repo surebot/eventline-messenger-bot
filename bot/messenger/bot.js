e0023aca46d0c8d302e53df42f4f69dec5b6537f
James Campbell <james@supmenow.com>
2017-08-28 13:53:30 +0100

    Fixes pattern being commented out.

--- bot/messenger/bot.js
+++ bot/messenger/bot.js
@@ -1,28 +1,12 @@
 const Eventline = require('eventline');
 import send from './messenger-api-helpers/send'
 
-// ===== COMPONENTS ============================================================
-// const ReadReceipt = require('./component/read-receipt')
-// const FetchMessengerProfile = require('./component/fetch-messenger-profile')
-// const GettingStarted = require('./component/getting-started')
-// const PersistentMenu = require('./component/persistent-menu')
-// const Notifications = require('./component/notifications')
-
 const eventline = new Eventline.Eventline()
 
-
-// ===== USE Components ========================================================
-//eventline.use(ReadReceipt) - TODO: Why does this module break things
-// eventline.use(PersistentMenu)
-// eventline.use(FetchMessengerProfile)
-// eventline.use(GettingStarted)
-
 // echo all of the user's messages
-eventline.on([
-    // {
-    //     'message.text': /.+/
-    // }
-])
+eventline.on({
+    'message.text': /.+/
+})
 .then(event => {
     console.log(event)
     console.log("=======")
